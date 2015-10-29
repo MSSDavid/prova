d1a3e7f95de1c55b57da5dfcfdca0b7c928ac21b
alunoinf <alunoinf@pc-649301.(none)>
2015-10-29 21:42:26 -0200

    enviando o pom.xml

---
+++ ModSimples/src/main/java/br/com/es/construcao/modsimples/ModSimples.java
@@ -0,0 +1,28 @@
+package br.com.es.construcao.modsimples;
+
+import br.com.es.construcao.divideSubt.DivideSubt;
+
+/**
+ *
+ * @author David Matheus
+ * 
+ */
+public class ModSimples {
+    
+    
+    public static int modsimples(int x, int y) {
+        
+        if (x < 0) {
+            throw new IllegalArgumentException("x: Menor que 0");            
+        }
+        
+        if (y < 0) {
+            throw new IllegalArgumentException("y: Menor que 0");            
+        }
+        
+        int d = DivideSubt.divideSubt(x, y);
+                
+        return x - (d * y);
+    }
+    
+}
