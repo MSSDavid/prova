2f9711c2f8c09adb2988967c3605e401b2d8529a
alunoinf <alunoinf@pc-649301.(none)>
2015-11-09 19:17:21 -0200

    Teste de Erros

--- Produto04/src/main/java/br/com/es/construcao/produto4/Produto.java
+++ Produto04/src/main/java/br/com/es/construcao/produto4/Produto.java
@@ -16,7 +16,7 @@ public class Produto {
             throw new IllegalArgumentException("b: menor que 0");
         }
         
-        int i = 1;
+        int i = 10;
         int s = 0;
         
         while ( i <= b) {
