2b174c2cec99a5c3991ad21a7cec62702ffa51cb
David Matheus <MSSDavid@users.noreply.github.com>
2017-04-17 20:36:15 -0300

    Update Fat17.java

--- Fatorial17/src/main/java/br/ufg/inf/es/construcao/fatorial17/Fat17.java
+++ Fatorial17/src/main/java/br/ufg/inf/es/construcao/fatorial17/Fat17.java
@@ -19,7 +19,7 @@ public class Fat17 {
         int f = 1;
                 
         while (i <= n) {
-            f = Produto.produto(i,i);
+            f = Produto.produto(f,i);
             i++;
         }
         
